f7f94870052707e5a5d24ba0375d6a68b2699911
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-12-15 17:20:34 -0300

    projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_usuario (first_name, last_name, email, password) VALUES ('Fufu', 'Brown', 'fufu@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_usuario (first_name, last_name, email, password) VALUES ('Maria', 'Joana', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
